aa8e6b9fe7f33e851d42751f3f5d2d774227980d
xiawu <xiawu@redhat.com>
2017-09-15 17:57:42 +0800

    lib/kdump.sh: disable avc message by default
    Signed-off-by: xiawu <xiawu@redhat.com>

--- lib/kdump.sh
+++ lib/kdump.sh
@@ -76,6 +76,9 @@ readonly K_LOCK_SSH_ID_RSA="${K_LOCK_AREA}/.ssh/id_rsa_kdump"
 readonly K_RETRY_COUNT=1000
 readonly K_CPU_THRESHOLD=8
 
+# Disable AVC Message
+export AVC_ERROR=${AVC_ERROR:-'+no_avc_check'}
+
 [[ "${KDEBUG,,}" == "true" ]] && set -x
 
 [ ! -d "${K_TMP_DIR}" ] &&  mkdir -p "${K_TMP_DIR}"
